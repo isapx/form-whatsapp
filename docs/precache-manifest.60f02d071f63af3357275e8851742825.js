44f101d66c1623030e8f3043ccce426e2e71cf61
Isap <isap@outlook.com>
2020-08-14 13:50:37 -0700

    Cambio de build a docs

---
+++ docs/precache-manifest.60f02d071f63af3357275e8851742825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b32af1c2a6376bae30bf7393447040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58fc617cc1712d31254f",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "97810e2f51c1ba1a393d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58fc617cc1712d31254f",
+    "url": "/static/js/2.2ee1756d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ee1756d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97810e2f51c1ba1a393d",
+    "url": "/static/js/main.de08fa09.chunk.js"
+  },
+  {
+    "revision": "0caa54e000a01fb675a5",
+    "url": "/static/js/runtime-main.bd746bb4.js"
+  }
+]);
